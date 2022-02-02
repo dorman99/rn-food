3326839a3497fe9bacd76c3c24fa3f3df60c2927
Hafizh Abdillah <hafizh.abdillah@flash-coffee.com>
2022-02-03 00:08:58 +0700

    add: pass props to controll child from parent

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,34 +1,41 @@
 import React from "react";
 import { View, StyleSheet, TextInput, Text } from "react-native";
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
-        <Feather name="search" color="black" style={styles.iconStyle} />
-        <TextInput style={styles.inputStyle} placeholder="search"/>
+      <Feather name="search" color="black" style={styles.iconStyle} />
+      <TextInput
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="search"
+        value={term}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    backgroundStyle: {
-      marginTop: 15,
-        backgroundColor: '#F0EEEE',
-        borderRadius: 5,
-        height: 50, 
-        marginHorizontal: 15,
-        flexDirection: 'row',
-    },
-    iconStyle: {
-      fontSize: 35,
-      alignSelf: 'center', // centeirng the items child
-      marginHorizontal: 10
-    },
-    inputStyle: {
-      fontSize: 18,
-      flex: 1 // to take the rest of space
-    }
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: "#F0EEEE",
+    borderRadius: 5,
+    height: 50,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center", // centeirng the items child
+    marginHorizontal: 10,
+  },
+  inputStyle: {
+    fontSize: 18,
+    flex: 1, // to take the rest of space
+  },
 });
 
 export default SearchBar;
